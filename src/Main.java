f3d3ec846d78dfba5cac284b24dfce8219efd6c2
LopesJosiel <josielslopes@gmail.com>
2023-07-06 20:43:10 -0300

    lendo entrada de dados

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner num1 = new Scanner(System.in);
+        System.out.println ("Escolha: somar s;subtrair - m; multiplicar x; dividir - d:\n");
+        // Lendo a entrada no teclado do total de números possíveis;
+        num1 = ler.nextString();
+        num1.nextLine(); // esvazia o buffer do teclado
+        double entrada = num1.nextDouble();
+        while ()
         System.out.println("Hello world!");
     }
 }
